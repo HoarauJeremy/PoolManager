c09bd6f79b6e6752a1b427fd1763793b7da35bf6
CheunRamassy <cheunramassy.pr@gmail.com>
2025-10-21 14:27:11 +0400

    Modification du fichier index, new, edit.html.twig et ClientType.php

--- src/Form/ClientType.php
+++ src/Form/ClientType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Client;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -20,7 +21,9 @@ class ClientType extends AbstractType
             ->add('code_postal')
             ->add('tel_fixe')
             ->add('tel_gsm')
-        ;
+            ->add('save', SubmitType::class, [
+                'label' => 'Ajouter un client',
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
