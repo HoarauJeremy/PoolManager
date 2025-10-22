f79a6d1dff4701d7eb02ae5b1cfa671bd213df75
Emjy <emjy.ahyong@gmail.com>
2025-10-22 10:26:10 +0400

    add tests unitaires

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -11,6 +11,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class RegistrationFormType extends AbstractType
 {
@@ -26,6 +27,19 @@ class RegistrationFormType extends AbstractType
                 'constraints' => [
                     new IsTrue([
                         'message' => 'Vous devez accepter nos conditions.',
+                    ]),
+                ],
+            ])
+            ->add('role', ChoiceType::class, [
+                'mapped' => false,
+                'choices' => [
+                    'Utilisateur' => 'ROLE_USER',
+                    'Administrateur' => 'ROLE_ADMIN',
+                ],
+                'label' => 'Rôle',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez sélectionner un rôle',
                     ]),
                 ],
             ])
